b8471f57b69596c805ade7aaf84fe94b069d9c94
TonyM1958 <tony.matthews1@outlook.com>
2020-04-19 11:43:06 +0100

    daily update

--- covid.py
+++ covid.py
@@ -1,7 +1,7 @@
 ##################################################################################################
 """
 Module:   Covid data analysis
-Date:     06 April 2020
+Date:     19 April 2020
 Author:   Tony Matthews
 """
 ##################################################################################################
@@ -16,13 +16,11 @@ Author:   Tony Matthews
 #              for larger terrortories where spread is slower. Over-ridden after peak cases occurs.
 # lag:         Expected number of days lag between peak cases and peak deaths. Default is 6 with
 #              typical values between 0 and 12. Over-ridden after peak deaths occurs.
-# r_cases:     Growth factor for cases sigmoid function 1 / (1 + exp(-rt)) used to fit the S
-#              curve to the observed data. t starts at -1 and ends at +1 as time goes from start to
-#              end day. Default is 7 with typically values between 6 and 9
-# r_deaths:    Growth factor for deaths sigmoid function, offset by lag. See above
-# contagious:  The number of days where a person may infect others. Default is 7 with typical values
-#              between 5 and 15
-# figwidth:    sets the size of the charts. Deault is 10 with typical values between 7 and 12.
+# spread:     The number of days to use when working out infection rate. Default is 7 with typical
+#              values between 5 and 15
+# dilation:    Controls the symmetry of the bell distribution. Default is 2 with typical values
+#              between 0.1 and 10. A value of 1 produces a symmetric distribution.
+# figwidth:    sets the size of the charts. Deault is 12 with typical values between 7 and 12.
 ##################################################################################################
 
 import json
@@ -31,7 +29,7 @@ import math
 import matplotlib.pyplot as plt
 
 json_data = None        # string containing json data downloaded from web site
-region_codes = ['UK', 'FR', 'IT', 'DE', 'ES', 'US', 'CN', 'KR', 'JP', 'NZ', 'RU']
+region_codes = ['UK', 'FR', 'IT', 'DE', 'ES', 'US', 'CN', 'JP', 'NZ', 'RU']
 
 def average(lst): 
     """
@@ -91,19 +89,17 @@ class Region :
     """
     Holds the data about a region
     """    
-    def __init__(self, geoId='UK', smooth=9, growth_days=38, lag=4, spread=7, dilation=2, r_cases=0, r_deaths=0, figwidth=12, debug=0) :
+    def __init__(self, geoId='UK', smooth=9, growth_days=38, lag=4, spread=7, dilation=2, figwidth=12, debug=0) :
         # check parameters
         self.debug = debug
         if geoId not in region_codes :
             print(f"Region Codes: {region_codes}\n")
-        if smooth % 2 == 0 : smooth += 1
+        if smooth % 2 == 0 : smooth += 1      # make sure the average is balanced around the centre point
         self.smooth = smooth                  # number of raw data points to average over to reduce anomalies
-        self.growth_days = growth_days        # predicted number of days from start to peak cases
-        self.lag = lag                        # predicted number of days lag between peak cases and peak deaths
-        self.r_cases = r_cases                # sigmoid range for cases e.g 1 + exp(-rt) for t = -1 to +1 
-        self.r_deaths = r_deaths              # sigmoid range for deaths e.g 1 + exp(-rt) for t = -1 to +1
-        self.spread = spread                  # number of days a person may infect others
-        self.dilation = dilation              # dilutes the drop off after peak
+        self.growth_days = growth_days        # assumed minimum days from start to peak cases
+        self.lag = lag                        # assumed minimum days between peak cases and peak deaths
+        self.spread = spread                  # number of days to use when working out infection rate
+        self.dilation = dilation              # controls distribution symmetry
         self.figsize = (figwidth, figwidth * 9 / 16)     # size of charts
         # load dat
         self.data = load(geoId)
@@ -395,17 +391,17 @@ class Region :
 
     def t (self, day, offset) :
         """
-        return the scaled time in a cycle for a day. Start t = -1 to End t = +1
-        dilation, if set, is applied after t = 0 to extend the decay time
+        return the scaled time from the day in the infection cycle, between t=-1 (s_start_days) to t=+1 (s_end_days)
+        dilation controls the symmetry of the distribution by manipulating time when t > 0.
         """
         lag = self.lag if offset == 1 else 0
         x = 2 * (day - self.s_start_days - lag) - self.cycle
-        if x > 0 : x /= self.dilation
+        if x > 0 and self.dilation != 1 : x /= self.dilation
         return x / self.cycle
 
     def bell_A(self, L, r, d, offset) :
         """
-        return points in the bell shaped distribution curve that is the derritative of the sigmoid function 
+        return a point in the scaled bell distribution using the derritative of the sigmoid function 
         """
         x = self.t(d, offset)
         A = L * math.exp(-1 * r * x) / (1 + math.exp(-1 * r * x)) ** 2
@@ -413,7 +409,7 @@ class Region :
 
     def bell_L(self, A, r, d, offset) :
         """
-        given a distribution point, work out L
+        given a point in the bell distribution, work out the scale factor L
         """
         x = self.t(d, offset)
         L = A * (1 + math.exp(-1 * r * x)) ** 2 / math.exp(-1 * r * x)
@@ -421,7 +417,7 @@ class Region :
 
     def abs_error(self, L, r, offset) :
         """
-        calculate the average absolute error between the smoothed data and bell distribution function:
+        calculate the average absolute error between the smoothed data and bell distribution for a given L and r:
         """
         name = 's_cases' if offset == 0 else 's_deaths'
         n = 0
@@ -439,33 +435,31 @@ class Region :
 
     def bell_r(self, L, r, offset, tries=0) :
         """
-        try to work out the best fit value for r
+        work out the best value for r
         """
         cases = 'Cases' if offset == 0 else 'Deaths'
-        n = 20
+        n = 16
         step = 2.0
         while step > 0.01 and n > 0 :
+            # get errors ordered by r is lower, same, higher
+            error = [self.abs_error(L, r - step, offset), self.abs_error(L, r, offset), self.abs_error(L, r + step, offset)]
+            if self.debug > 1 : print(f"r={r}, step={step}, error = {error}")
+            direction = error.index(min(error)) - 1     # direction of lowest error is -1, 0, +1 steps
+            r += direction * step
+            if direction == 0 : step /= 2
+            # limit stops to prevent run-away
+            if r < 4.0 : r = 4.0
+            if r > 8.0 : r = 8.0
             n -= 1
-            x0 = self.abs_error(L, r, offset)
-            x1 = self.abs_error(L, r + step, offset)
-            x2 = self.abs_error(L, r - step, offset)
-            if x1 < x0 and x1 < x2 :
-                r += step
-            elif x2 < x0 and x2 < x1 :
-                r -= step
-            else :
-                step /= 2
         if self.debug > 0 : print(f"> {cases} {tries}: L = {int(L):,}, r = {round(r, 2)}")
-        if r < 4.0 : return 4.0
-        if r > 8.0 : return 8.0
         return r
 
     def fit_cases(self, day) :
-        # fit L_cases and r_cases to smoothed data
+        # fit L_cases and r_cases to the smoothed data
         previous_L = 0.0
         previous_r = 0.0
         tries = 0
-        if self.r_cases == 0 : self.r_cases = 6
+        if self.r_cases is None : self.r_cases = 6
         while tries < 10 :
             self.L_cases = self.bell_L(self.data[day].get('s_cases'), self.r_cases, day, 0)
             self.r_cases = self.bell_r(self.L_cases, self.r_cases, 0, tries)
@@ -473,7 +467,7 @@ class Region :
             previous_L = int(self.L_cases)
             previous_r = round(self.r_cases,2)
             tries += 1
-        if tries >= 20 : print(f"** fit_cases did not converge")
+        if tries >= 20 : print(f"** fit_cases was not solved")
         return
         
     def fit_deaths(self, day) :
@@ -481,7 +475,7 @@ class Region :
         previous_L = 0.0
         previous_r = 0.0
         tries = 0
-        if self.r_deaths == 0 : self.r_deaths = 6
+        if self.r_deaths is None : self.r_deaths = 6
         while tries < 10 :
             self.L_deaths = self.bell_L(self.data[day].get('s_deaths'), self.r_deaths, day, 1)
             self.r_deaths = self.bell_r(self.L_deaths, self.r_deaths, 1, tries)
@@ -489,22 +483,23 @@ class Region :
             previous_L = int(self.L_deaths)
             previous_r = round(self.r_deaths, 2)
             tries += 1
-        if tries >= 20 : print(f"** fit_deaths did not converge")
+        if tries >= 20 : print(f"** fit_deaths was not solved")
         return
 
     def build_bell(self) :
         """
-        use a sigmoid population function to model the smoothed total number of cases / deaths.
-        The function is y = L / (1 + exp(-rt)) where t = -1 at the start and +1 at the end. This produces
-        an S curve that starts at 0 and tends to L at the end. The differential of the S curve is a bell
-        curve that describes the daily changes in cases / deaths. Both curves are held from start day.
+        Build a bell distribution curve model for the smoothed number of new cases / deaths.
+        This is the derrivative of the sigmoid population function A = L / (1 + exp(-rt))
+        A = L * exp(-rt) / (1 + exp(-rt)) ** 2
         """
         self.bell_cases = []
         self.bell_cases_to_date = []
         self.bell_deaths = []
         self.bell_deaths_to_date = []
-        self.L_cases = None
-        self.L_deaths = None
+        self.L_cases = None             # scale factor for cases
+        self.r_cases = None             # r factor for cases
+        self.L_deaths = None            # scale factor for deaths
+        self.r_deaths = None            # r factor for deaths
         if self.s_total_deaths < 50 :
             return
         if self.s_peak_case_days < self.s_latest_days : d = self.s_peak_case_days
